e7d663299340aa7735817af6bfa150d4c2711bc5
tatsuya-hurex <83265709+tatsuya-hurex@users.noreply.github.com>
2022-11-24 15:30:44 +0900

    add-text & not yet display text

--- src/App.js
+++ src/App.js
@@ -1,30 +1,46 @@
 import {useState, useEffect } from 'react';
 
+const yobiText = [
+  {id: 1, text: "aaa"},
+  {id: 2, text: "bbb"},
+  {id: 3, text: "ccc"},
+  {id: 5, text: "eee"},
+  {id: 6, text: "fff"},
+  {id: 7, text: "ggg"},
+]
 
+//ランダム整数の生成関数
+const getRandomInt = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+}
+
+//配列の数
+const argLength = yobiText.length;
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [words, setWords] = useState([
-    {id: 1, text: "aaa"},
-    {id:2, text: "bbb"},
-    {id:3, text: "ccc"},
-  ]);
+  const [words, setWords] = useState([]);
   const [timer, setTimer] = useState(false);
 
-  const countup = () => {
-    setCount(count => count + 1);
-  };
 
-  const addText = () => {
-    const newWords = [...words, {id: 4, text: "ddd"}]
-    console.log(newWords)
+  //秒間処理
+  const secFun = () => {
+    const randomNum = getRandomInt(0,argLength)
+    const newWords = [...words, yobiText[randomNum]]
+    // const newWords = [...words, {id: 4, text: "ddd"}] //スプレッド構文で展開して追加
     setWords(newWords)
-    console.log(words)
   }
+
+
+  useEffect(() => {
+    setTimer(true)
+
+  },[])
 
   useEffect(() => {
     if (timer) {
-      const timerId = setInterval(addText, 1000);
+      const timerId = setInterval(secFun, 1000);
       return () => clearInterval(timerId);
     }
   }, [timer]);
@@ -39,7 +55,6 @@ function App() {
       ))}</div>
       <button onClick={() => setTimer(true)}>スタート</button>
       <button onClick={() => setTimer(false)}>ストップ</button>
-      <button onClick={() => setCount(0)}>リセット</button>
     </div>
   );
 }
